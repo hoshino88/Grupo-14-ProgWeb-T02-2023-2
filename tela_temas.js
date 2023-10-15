f8ba217c51caeab8fc95bee3a49153b98accfd7f
tiago <tiago.guidorizzi@gmail.com>
2023-10-15 15:56:59 -0400

    a

---
+++ tela_temas.js
@@ -0,0 +1,13 @@
+const sidebar = document.getElementById("sidebar");
+const imgSanduiche = document.getElementById("imgSanduiche");
+const overlay = document.getElementById("overlay");
+
+imgSanduiche.onclick = () => {
+    sidebar.classList.toggle("active");
+    overlay.classList.toggle("hidden");
+}
+
+overlay.onclick = () => {
+    sidebar.classList.toggle("active");
+    overlay.classList.toggle("hidden");
+}
