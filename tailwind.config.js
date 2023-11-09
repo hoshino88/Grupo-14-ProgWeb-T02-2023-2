01e0bfef28a60dc78c036ec1c33a0b5613deb8f2
tiago <tiago.guidorizzi@gmail.com>
2023-11-09 18:30:37 -0400

    removendo tailwind

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["C:\Users\User\Documents\Grupo-14-ProgWeb-T02-2023-2-1.{html, js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
